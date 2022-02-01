80bcb50afe4535f86d3122b5a922bdaa7afe853c
Wojciech <olowek92@gmail.com>
2022-02-01 13:05:57 +0100

    setup-for-Prettier

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App(): JSX.Element {
-  return (
-    <h1>Siema</h1>
-  );
+    return <h1>Siema</h1>
 }
 
-export default App;
+export default App
