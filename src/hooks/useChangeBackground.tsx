505173bc16497ce8f86384df577f8eb826fa4696
Wojciech <olowek92@gmail.com>
2022-02-17 10:06:13 +0100

    background-images-different-screens-part2

--- src/hooks/useChangeBackground.tsx
+++ src/hooks/useChangeBackground.tsx
@@ -2,22 +2,26 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { MenuData } from 'data/MenuData';
 
+const desktop = window.matchMedia('(min-width: 992px)');
+
 export const useChangeBackground = ():void => {
-    const location = useLocation();
+  const location = useLocation();
   
     useEffect(():void => {
-      MenuData.map(( item ) => {
-        if(location.pathname === item.path) {
-          document.body.style.backgroundImage = `url(${item.background})`;
-        } else {
-          item.submenu 
-          ? item.submenu.map(( subItem ) => {
-            if(location.pathname === subItem.path) {
-              document.body.style.backgroundImage = `url(${subItem.background})`;
-            }
-          }) 
-          : null;
-        }
-      })
-    }, [location])
+      if(desktop.matches) {
+        MenuData.map(( item ) => {
+          if(location.pathname === item.path) {
+            document.body.style.backgroundImage = `url(${item.background})`;
+          } else {
+            item.submenu 
+            ? item.submenu.map(( subItem ) => {
+              if(location.pathname === subItem.path) {
+                document.body.style.backgroundImage = `url(${subItem.background})`;
+              }
+            }) 
+            : null;
+          }
+        })
+      }
+    }, [location])    
   }
